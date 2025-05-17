65262a46f3e0a7a93c42c5d8447998f9fed85ad4
Vaibhav Chopde <vchopde77@gmail.com>
2025-05-17 17:32:43 +0530

    fix: fixing cookies

--- utils/setSecureCookies.js
+++ utils/setSecureCookies.js
@@ -1,6 +1,8 @@
 function setSecureCookies(res, token) {
   res.cookie("access_token", token, {
+    path: "/",
     httpOnly: true,
+    sameSite: "None", // this is crucial for cross-site
     maxAge: process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
     secure: process.env.NODE_ENV === "production",
   });
