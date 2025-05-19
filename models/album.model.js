1fff41e5e74ac71c7f5d6eb9d766cd454380763a
Vaibhav Chopde <vchopde77@gmail.com>
2025-05-19 08:07:59 +0530

    fix: added timestamps to album model

--- models/album.model.js
+++ models/album.model.js
@@ -1,23 +1,28 @@
 const mongoose = require("mongoose");
 const { randomUUID } = require("crypto");
 
-const albumSchema = new mongoose.Schema({
-  _id: {
-    type: mongoose.Schema.Types.UUID,
-    default: () => randomUUID(),
+const albumSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: mongoose.Schema.Types.UUID,
+      default: () => randomUUID(),
+    },
+    name: {
+      type: String,
+      required: [true, "name field cannot be empty"],
+    },
+    description: { type: String },
+    ownerId: {
+      type: mongoose.Schema.Types.UUID,
+      ref: "User",
+      required: true,
+    },
+    sharedUsers: { type: [String] },
   },
-  name: {
-    type: String,
-    required: [true, "name field cannot be empty"],
-  },
-  description: { type: String },
-  ownerId: {
-    type: mongoose.Schema.Types.UUID,
-    ref: "User",
-    required: true,
-  },
-  sharedUsers: { type: [String] },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Album = mongoose.model("Album", albumSchema);
 
