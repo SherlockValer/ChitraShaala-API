976e82817937c1d62298096cdcb4800519bf483c
Vaibhav Chopde <vchopde77@gmail.com>
2025-06-23 10:14:28 +0530

    fix: fixed previous commit errors

--- utils/removeSharedUsers.js
+++ utils/removeSharedUsers.js
@@ -1,6 +1,6 @@
 const Album = require("../models/album.model.js");
 
-async function removeSharedUsers(album, userId) {
+export async function removeSharedUsers(album, userId) {
   if (album.ownerId !== userId) {
     const res = await Album.findById(album._id, { sharedUsers: 0 });
     return res;
